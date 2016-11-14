70b5e59557a9e6e5c7239b2f96f3edd3a962c671
grigory <sss>
2016-11-14 18:41:08 +0300

    Добавлена страница статистики

--- NameThem.py
+++ NameThem.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask import url_for, render_template, request, redirect
+from time import *
+import pickle
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def form():
+    # Дата и время для регистрации анкеты
+    ticks = time()
+    lt = localtime(ticks)
+    ddNow = str(lt.tm_mday)
+    mmNow = str(lt.tm_mon)
+    yyyyNow = str(lt.tm_year)
+    hourNow = str(lt.tm_hour)
+    minNow = str(lt.tm_min)
+
     if request.args:
         # Анкета
         name = request.args['name']
@@ -18,9 +29,40 @@ def form():
         coffeemach = request.args['coffeemach']
         television = request.args["television"]
         kettle = request.args["kettle"]
-        print(name, lastname, age, sex, residence, language)
-        return render_template('aform.html', name=name, birth=age)
+        #print(name, lastname, age, sex, residence, language)
+        f = open('form' + mmNow + ddNow + hourNow + minNow, 'wb')
+        questdic = {'Name': name, 'Lastname': lastname, 'Age': age, 'Sex': sex,
+                    'Residence':residence, 'Language':language, 'Coffee':coffeemach,
+                    'Television':television, 'Kettle':kettle}
+        pickle.dump(questdic, f)
+        f.close()
+        return render_template('Thanks.html', name=name, birth=age, day=ddNow, month=mmNow)
     return render_template('NameThings.html')
+
+@app.route('/stats')
+def stats():
+    import glob, os
+    os.chdir("./")
+    count = 0
+    cmales = 0
+    cfemales = 0
+    ccity = 0
+    cvillage = 0
+    for file in glob.glob("form*"):
+        count = count + 1
+        f = open(file, 'rb')
+        dic = pickle.load(f)
+        if dic['Sex'] == 'male':
+            cmales += 1
+        else:
+            cfemales += 1
+        if dic['Residence'] == 'city':
+            ccity += 1
+        else:
+            cvillage += 1
+
+    return render_template('Stats.html', count=count, cmales=cmales, cfemales=cfemales, ccity=ccity, cvillage=cvillage)
+
 
 
 if __name__ == '__main__':
